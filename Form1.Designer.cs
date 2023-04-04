9e4e44953a325cf75d0f32eb61f6bec171dc2ebb
Fahrican Kaçan <fahri_can_98@hotmail.com>
2023-04-04 18:22:47 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(textBox1);
+            resources.ApplyResources(panel1, "panel1");
+            panel1.Name = "panel1";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(textBox1, "textBox1");
+            textBox1.Name = "textBox1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel1);
+            Name = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox textBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using NCalc;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        private static string[] sciencificOperationsArr =
+            {
+                 "x^y", "√", "³√", "sin", "cos",
+                         "tan", "log", "ln", "e^x", "10^x",
+                         "x!", "mod", "|x|", "π", "e",
+                         "sin⁻¹", "cos⁻¹", "tan⁻¹", "sinh", "cosh",
+                         "tanh", "Ran", "Deg", "Rad", "Grad"
+            };
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateCalculatorButtons();
+
+        }
+
+        private void CreateCalculatorButtons()
+        {
+            string[] buttonTexts = { "7", "8", "9", "/", "C",
+                         "4", "5", "6", "*", "±",
+                         "1", "2", "3", "-", "(",
+                         "0", ".", "=", "+", ")",
+                         "x^y", "√", "³√", "sin", "cos",
+                         "tan", "log", "ln", "e^x", "10^x",
+                         "x!", "mod", "|x|", "π", "e",
+                         "sin⁻¹", "cos⁻¹", "tan⁻¹", "sinh", "cosh",
+                         "tanh", "Ran", "Deg", "Rad", "Grad"};
+
+
+            int buttonWidth = 50;
+            int buttonHeight = 50;
+            int padding = 10;
+            int currentX = 0;
+            int currentY = 0;
+            int rowCount = 1;
+            for (int i = 0; i < buttonTexts.Length; i++)
+            {
+                Button button = new Button();
+                button.Text = buttonTexts[i];
+                button.Width = buttonWidth;
+                button.Height = buttonHeight;
+                button.Location = new Point(currentX, currentY);
+                button.Click += Button_Click;
+
+                panel1.Controls.Add(button);
+
+                currentX += buttonWidth + padding;
+
+                if ((i + 1) % 5 == 0)
+                {
+                    rowCount++;
+                    currentX = 0;
+                    currentY += buttonHeight + padding;
+                }
+            }
+
+            textBox1.Location = new Point(0, currentY);
+            textBox1.Width = ((buttonWidth + padding) * 5) - padding;
+
+            this.Width = textBox1.Width + padding * 2 - 4;
+            this.Height = rowCount * (padding + buttonHeight) + padding;
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string buttonText = button.Text;
+
+            if (sciencificOperationsArr.Any(x => x == buttonText))
+            {
+                double calculatedResutl = equalButtonOperations();
+                if (buttonText == "sin")
+                    textBox1.Text = Math.Sin(Convert.ToDouble(calculatedResutl)).ToString();
+                if (buttonText == "cos")
+                    textBox1.Text = Math.Cos(Convert.ToDouble(calculatedResutl)).ToString();
+            }
+            else
+            {
+                StandartCalculator(buttonText);
+
+            }
+        }
+
+        private void StandartCalculator(string buttonText)
+        {
+            switch (buttonText)
+            {
+                case "C":
+                    textBox1.Text = "";
+                    break;
+                case "=":
+                    try
+                    {
+                        equalButtonOperations();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Hatalı ifade: " + ex.Message);
+                    }
+                    break;
+                case "±":
+                    if (!string.IsNullOrEmpty(textBox1.Text))
+                    {
+                        if (textBox1.Text.StartsWith("-"))
+                            textBox1.Text = textBox1.Text.Substring(1);
+                        else
+                            textBox1.Text = "-" + textBox1.Text;
+                    }
+                    break;
+                default:
+                    textBox1.Text += buttonText;
+                    break;
+            }
+        }
+
+        private double equalButtonOperations()
+        {
+            char lastChar = textBox1.Text[textBox1.Text.Length - 1];
+            if (!char.IsDigit(lastChar))
+            {
+                textBox1.Text=textBox1.Text.Substring(0,textBox1.Text.Length - 1);
+            }
+            DataTable table = new DataTable();
+            var str =  textBox1.Text.Replace(',','.');
+            table.Columns.Add("expression", typeof(string), str);
+            DataRow row = table.NewRow();
+            table.Rows.Add(row);
+            double result = double.Parse((string)row["expression"]);
+            textBox1.Text = result.ToString();
+            return Convert.ToDouble(textBox1.Text);
+        }
+
+        public  string GetUntilOrEmpty( string text, string stopAt = "-")
+        {
+            if (!String.IsNullOrWhiteSpace(text))
+            {
+                int charLocation = text.IndexOf(stopAt, StringComparison.Ordinal);
+
+                if (charLocation > 0)
+                {
+                    return text.Substring(0, charLocation);
+                }
+            }
+
+            return String.Empty;
+        }
+    }
+}

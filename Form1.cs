977f7682c8e844a59fb52a12c1e868adbee0d793
Fahrican <fahrican.kacan@gmail.com>
2023-04-20 02:37:45 +0300

    Bilimsel hesaplamalr eklendi

--- Form1.cs
+++ Form1.cs
@@ -33,11 +33,11 @@ namespace Calculator
                          "4", "5", "6", "*", "±",
                          "1", "2", "3", "-", "(",
                          "0", ".", "=", "+", ")",
-                         "x^y", "√", "³√", "sin", "cos",
+                         "√", "³√", "sin", "cos",
                          "tan", "log", "ln", "e^x", "10^x",
                          "x!", "mod", "|x|", "π", "e",
                          "sin⁻¹", "cos⁻¹", "tan⁻¹", "sinh", "cosh",
-                         "tanh", "Ran", "Deg", "Rad", "Grad"};
+                         "tanh",  "Deg", "Rad"};
 
 
             int buttonWidth = 50;
@@ -67,11 +67,11 @@ namespace Calculator
                 }
             }
 
-            textBox1.Location = new Point(0, currentY);
+            textBox1.Location = new Point(0, currentY+60);
             textBox1.Width = ((buttonWidth + padding) * 5) - padding;
 
-            this.Width = textBox1.Width + padding * 2 - 4;
-            this.Height = rowCount * (padding + buttonHeight) + padding;
+            this.Width = textBox1.Width + 100 + padding * 2 - 4;
+            this.Height = rowCount * (padding + buttonHeight) + padding + 120;
             this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
         }
 
@@ -83,10 +83,7 @@ namespace Calculator
             if (sciencificOperationsArr.Any(x => x == buttonText))
             {
                 double calculatedResutl = equalButtonOperations();
-                if (buttonText == "sin")
-                    textBox1.Text = Math.Sin(Convert.ToDouble(calculatedResutl)).ToString();
-                if (buttonText == "cos")
-                    textBox1.Text = Math.Cos(Convert.ToDouble(calculatedResutl)).ToString();
+                ScienceficOperations(buttonText, calculatedResutl);
             }
             else
             {
@@ -95,56 +92,97 @@ namespace Calculator
             }
         }
 
+        private void ScienceficOperations(string buttonText, double calculatedResutl)
+        {
+            if (buttonText == "sin") textBox1.Text = Math.Sin(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "sinh") textBox1.Text = Math.Sinh(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "sin⁻¹") textBox1.Text = Math.Asin(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "cos") textBox1.Text = Math.Cos(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "cosh") textBox1.Text = Math.Cosh(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "cos⁻¹") textBox1.Text = Math.Acos(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "tan⁻¹") textBox1.Text = Math.Atan(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "mod")
+            {
+                if (textBox1.Text.Trim().Length <= 0) MessageBox.Show("Plesease give an number");
+                else textBox1.Text += "%";
+            }
+            if (buttonText == "tan") textBox1.Text = Math.Tan(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "tanh") textBox1.Text = Math.Tanh(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "π") textBox1.Text = Math.PI.ToString();
+            if (buttonText == "e") textBox1.Text = Math.E.ToString();
+            if (buttonText == "log") textBox1.Text = Math.Log(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "ln") textBox1.Text = Math.Log(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "e^x") textBox1.Text = Math.Exp(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "10^x") textBox1.Text = Math.Pow(10, Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "|x|") textBox1.Text = Math.Abs(Convert.ToDouble(calculatedResutl)).ToString();
+            if (buttonText == "x!") textBox1.Text = CalculateFactorial(Convert.ToInt32(calculatedResutl)).ToString();
+            if (buttonText == "³√") textBox1.Text = Math.Pow(Convert.ToInt32(calculatedResutl), 1.0 / 3.0).ToString();
+            if (buttonText == "√") textBox1.Text = Math.Pow(Convert.ToInt32(calculatedResutl), 1.0 / 2.0).ToString();
+            if (buttonText == "Rad") textBox1.Text = (Convert.ToInt32(calculatedResutl) * (Math.PI / 180)).ToString();
+            if (buttonText == "Deg") textBox1.Text = (Convert.ToInt32(calculatedResutl) * (180 / Math.PI)).ToString(); ;
+        }
+
         private void StandartCalculator(string buttonText)
         {
-            switch (buttonText)
+            try
             {
-                case "C":
-                    textBox1.Text = "";
-                    break;
-                case "=":
-                    try
-                    {
-                        equalButtonOperations();
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("Hatalı ifade: " + ex.Message);
-                    }
-                    break;
-                case "±":
-                    if (!string.IsNullOrEmpty(textBox1.Text))
-                    {
-                        if (textBox1.Text.StartsWith("-"))
-                            textBox1.Text = textBox1.Text.Substring(1);
-                        else
-                            textBox1.Text = "-" + textBox1.Text;
-                    }
-                    break;
-                default:
-                    textBox1.Text += buttonText;
-                    break;
+                switch (buttonText)
+                {
+                    case "C":
+                        textBox1.Text = "";
+                        break;
+                    case "=":
+                        double a = equalButtonOperations();
+                        //if (a == 0) MessageBox.Show("Please give an input");
+                        break;
+                    case "±":
+                        if (!string.IsNullOrEmpty(textBox1.Text))
+                        {
+                            if (textBox1.Text.StartsWith("-"))
+                                textBox1.Text = textBox1.Text.Substring(1);
+                            else
+                                textBox1.Text = "-" + textBox1.Text;
+                        }
+                        break;
+                    default:
+                        textBox1.Text += buttonText;
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hatalı ifade: " + ex.Message);
             }
         }
 
         private double equalButtonOperations()
         {
-            char lastChar = textBox1.Text[textBox1.Text.Length - 1];
-            if (!char.IsDigit(lastChar))
+            try
             {
-                textBox1.Text=textBox1.Text.Substring(0,textBox1.Text.Length - 1);
+                char lastChar = textBox1.Text[textBox1.Text.Length - 1];
+                if (!char.IsDigit(lastChar))
+                {
+                    textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+                }
+                DataTable table = new DataTable();
+                var str = textBox1.Text.Replace(',', '.');
+                table.Columns.Add("expression", typeof(string), str);
+                DataRow row = table.NewRow();
+                table.Rows.Add(row);
+                double result = double.Parse((string)row["expression"]);
+                textBox1.Text = result.ToString();
+                return Convert.ToDouble(textBox1.Text);
             }
-            DataTable table = new DataTable();
-            var str =  textBox1.Text.Replace(',','.');
-            table.Columns.Add("expression", typeof(string), str);
-            DataRow row = table.NewRow();
-            table.Rows.Add(row);
-            double result = double.Parse((string)row["expression"]);
-            textBox1.Text = result.ToString();
-            return Convert.ToDouble(textBox1.Text);
+            catch
+            {
+                MessageBox.Show("Düzgün gir lan");
+                return 0;
+
+            }
+
         }
 
-        public  string GetUntilOrEmpty( string text, string stopAt = "-")
+        public string GetUntilOrEmpty(string text, string stopAt = "-")
         {
             if (!String.IsNullOrWhiteSpace(text))
             {
@@ -157,6 +195,25 @@ namespace Calculator
             }
 
             return String.Empty;
+        }
+        public int CalculateFactorial(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * CalculateFactorial(n - 1);
+            }
+        }
+
+        public class Sukro
+        {
+            public string Name { get; set; }
+            public int Kol { get; set; } = 31;//cm
+            public bool Damarlimi { get; set; } = true;
+            public int Bacak { get; set; } = 56;//cm
         }
     }
 }
